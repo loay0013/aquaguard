fda3c3e5b11ff3c86edf7cff16ed25cf84ebc8cf
MikkelLebedev <mikkel.lebedev@outlook.com>
2022-12-05 14:07:00 +0100

    LOL

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,29 @@
 <footer>
-	<div class="background">
-		<div class="row">
+	<div class="background-footer">
+		<div class="row m-0">
+			<div class="col-12 d-flex justify-content-center align-items-center py-5 p-0">
+				<img src="img/aquaguard_logo_white.svg" class="m-0 w-75 h-75">
+			</div>
+			<div class="col-12">
+				<div class="d-flex justify-content-center my-3 gap-4 align-items-center">
+					<svg xmlns="http://www.w3.org/2000/svg" width="20px" height="22px" viewBox="0 0 18.855 18.892">
+						<path id="Icon_feather-phone" data-name="Icon feather-phone" d="M22.022,17.144v2.844a1.9,1.9,0,0,1-2.067,1.9,18.761,18.761,0,0,1-8.181-2.91,18.486,18.486,0,0,1-5.688-5.688,18.761,18.761,0,0,1-2.91-8.219A1.9,1.9,0,0,1,5.062,3H7.906A1.9,1.9,0,0,1,9.8,4.631a12.172,12.172,0,0,0,.664,2.664,1.9,1.9,0,0,1-.427,2l-1.2,1.2a15.168,15.168,0,0,0,5.688,5.688l1.2-1.2a1.9,1.9,0,0,1,2-.427,12.173,12.173,0,0,0,2.664.664A1.9,1.9,0,0,1,22.022,17.144Z" transform="translate(-3.168 -3)" fill="#fff"/>
+					</svg>
+					<p class="text-white footer-contact">+ 45 55 55 55 55</p>
+				</div>
+				<div class="d-flex justify-content-center my-3 gap-3 align-items-center">
+					<svg xmlns="http://www.w3.org/2000/svg" width="22px" height="22px" viewBox="0 0 18.927 15.142">
+						<path id="Icon_material-email" data-name="Icon material-email" d="M20.035,6H4.893A1.89,1.89,0,0,0,3.009,7.893L3,19.249a1.9,1.9,0,0,0,1.893,1.893H20.035a1.9,1.9,0,0,0,1.893-1.893V7.893A1.9,1.9,0,0,0,20.035,6Zm0,3.785-7.571,4.732L4.893,9.785V7.893l7.571,4.732,7.571-4.732Z" transform="translate(-3 -6)" fill="#fff"/>
+					</svg>
+					<p class="text-white footer-contact">info@aquaguard.dk</p>
+				</div>
+				<div class="col-12 d-flex justify-content-center py-4">
+					<div class="line"></div>
+				</div>
+			</div>
+		</div>
+
+		<div class="row m-0">
 			<div class="col-12"></div>
 		</div>
 	</div>
