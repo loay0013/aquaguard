8199b1b148b5be75f5c3573daa9c8c386dd1f3ed
loayhajjo <louy0013@edu.zealand.dk>
2022-12-06 10:08:24 +0100

    index header 1.1

--- includes/nav.php
+++ includes/nav.php
@@ -1,13 +1,15 @@
 <nav class="navbar navbar-expand-lg navbar-light ">
     <div class="container-fluid">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
         <a class="navbar-brand" href="#">
             <img class="m-3" src="img/aquaguard_logo.svg" alt="" width="200" height="100">
         </a>
-        <div class="collapse navbar-collapse d-flex justify-content-center" id="navbarTogglerDemo03">
+        <div class="collapse navbar-collapse d-flex justify-content-center" id="navbarNav">
             <ul class="navbar-nav d-flex me-lg-5 ">
                 <li class="nav-item m-3">
                     <a class="nav-link active" aria-current="page" href="#">Forside</a>
--- index.php
+++ index.php
@@ -18,8 +18,8 @@
     <meta name="copyright" content="Information om copyright">
 
     <!-- Sikrer man kan benytte CSS ved at tilkoble en CSS fil -->
-    <link href="css/styles.css" rel="stylesheet" type="text/css">
     <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
 
 
     <!-- Sikrer den vises korrekt på mobil, tablet mv. ved at tage ift. skærmstørrelse - bliver brugt til responsive websider -->
@@ -30,11 +30,11 @@
 <body class="h-100">
 <?php include "includes/nav.php"; ?>
 
-<header>
+<header class=" h-100 position-relative overflow">
     <div class="video">
 
         <video playsinline autoplay muted loop id="myVideo" class="d-block position-absolute">
-            <source src="img/">
+            <source src="img/watertap.mp4">
         </video>
     </div>
 
@@ -95,6 +95,113 @@
 
 <section>
 
+    <div>
+        <img src="img/mockup.png" alt="app">
+    </div>
+
+   <div>
+        <h2>
+            Aquaguard-appen
+        </h2>
+       <p>
+           En helt ny app til dit Aquaguard-filter. De mange features giver dig
+           et godt overblik over din hustands vandforbrug. Du får det hele samlet
+           på ét sted med lækkert design.
+       </p>
+       <p>
+           Aquaguard-appen er udviklet specielt til dig. I appen kan du registrere,
+           hvor i dit hus du har filtrene siddende på vandhaner og brusere. Du kan se, hvor
+           meget vand du forbruger i de forskellige rum i dit hus, og hvor mange penge,
+           du har sparet i dag, på en uge, eller du kan tracke for en længere periode.
+       </p>
+
+       <div>
+            <img src="img/app%20store.svg" alt="app store">
+            <img src="img/google%20play.svg" alt="google play">
+       </div>
+   </div>
+
+</section>
+
+
+
+<section>
+
+    <div>
+
+        <div>
+            <div>
+               <img src="img/returret.svg">
+                <p>14 dages returret</p>
+                <p>Du får altid 14 dages returret.
+                    Det synes vi er god service.</p>
+            </div>
+
+            <div>
+                <img src="img/betaling.svg">
+                <p>Fleksibel betaling</p>
+                <p>Du vælger mellem en række
+                    forskellige betalingsmuligheder.</p>
+            </div>
+
+            <div>
+                <img src="img/levering.svg">
+                <p>Gratis levering</p>
+                <p>Du får gratis levering,
+                    når du køber for 500 DKK.</p>
+            </div>
+
+            <div>
+                <img src="img/support.svg">
+                <p>Online support</p>
+                <p>Vi tilbyder gratis online support,
+                    hvor du kan stille spørgsmål.</p>
+            </div>
+
+        </div>
+
+    </div>
+
+</section>
+
+<section>
+    <div>
+        <div>
+            <img src="img/jacek-dylag-Vve7XkiUq_Y-unsplash.png">
+        </div>
+        <div>
+            <h3>
+            Abonner på Aquaguards nyhedsbrev
+            og få nyheder og tips.
+            </h3>
+
+            <input class="label-kontakt" type="email" placeholder="Din email">
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                Launch demo modal
+            </button>
+
+            <!-- Modal -->
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            ...
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
 
 </section>
 
@@ -104,11 +211,10 @@
 
 
 
-
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 
 <?php include "includes/footer.php"; ?>
 
-<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
 </html>
