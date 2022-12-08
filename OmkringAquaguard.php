2ecf8416bac0c5f9f52fdf67e3e4a45cd2a237ce
MikkelLebedev <mikkel.lebedev@outlook.com>
2022-12-08 13:45:18 +0100

    Omkring os sektion med billeder og tekst

--- OmkringAquaguard.php
+++ OmkringAquaguard.php
@@ -26,20 +26,97 @@
 </head>
 
 <!-- i <body> har man alt indhold på siden som brugeren kan se -->
-<body class="h-100">
+<body>
 <?php include "includes/nav.php"?>
 
+<section class="">
+	<div class="hero-background">
+		<img class="image-1" src="img/omoswatertap.png" alt="water-tap">
+	</div>
+</section>
 
-<section class="hero-background">
-	<div  class="d-flex justify-content-center">
-		<div class="w-50">
-			<img class="image-1" src="img/water-tap-2.jpg" alt="water-tap">
+<section class="mx-3 mx-md-5">
+	<div class="row mx-md-5">
+		<div class="col-12 mt-5">
+			<div class="text-center mx-md-5 text-md-start py-5 pt-4">
+				<h1 class="px-1">Om Aquaguard</h1>
+				<h2 class="px-1">Kontrol over dit vandbrug</h2>
+			</div>
+			<p class="fs-6 col-12 col-md-8 mx-md-5">Når du køber filtre fra Aquaguard køber du lækkert design. I vores app kan du følge med i dit vandforbrug, så du kan spare penge på dine vandregninger. På den måde kan du bruge dine penge på det du helst vil. Du får en gadgets, du sparer penge, og du tager miljøhensyn.</p>
 		</div>
 	</div>
-
-
-
 </section>
+
+<section class="mx-3 mx-md-5">
+	<div class="row mx-md-5">
+		<div class="col-12 pb-3">
+			<div class="text-center mx-md-5 text-md-start my-5">
+				<h2 class="px-1">Kontrol over dit vandbrug</h2>
+			</div>
+			<p class="fs-6 col-12 col-md-8 mx-md-5">Aquaguard er en dansk virksomhed, som er stiftet af 5 innovative iværksættere med multimediedesign baggrund. Idéen opstod, fordi både behovet for at spare penge og passe på miljøet er stort i en tid som vores. Med lækkert design i fokus fik vi kontakt til de leverandører, som vi har en dag, og sammen begyndte vi at udvikle de filtre, som kom til at hedde Aquaguard. Vores mission er, at det gøre let og interessant at spare penge og passe på vores miljø.</p>
+		</div>
+	</div>
+</section>
+
+<section class="mx-4">
+		<div class="d-flex justify-content-center">
+			<div class="d-flex flex-column flex-md-row py-4 text-center w-88 px-md-3">
+				<div class="py-3 col-12 col-md-4 ">
+					<svg class="pb-4" xmlns="http://www.w3.org/2000/svg" width="99.029" height="129.083" viewBox="0 0 99.029 129.083">
+						<g id="quality-3602" transform="translate(-30.219 0.006)">
+							<path id="Path_51" data-name="Path 51" d="M160.09,18.22c-14.82-2.695-27.74-8.261-38.4-16.543A7.994,7.994,0,0,0,116.78-.006c-13.288,43.511-13.6,86.549,0,129.083a11.731,11.731,0,0,0,3.673-.588c28.089-9.331,46.238-31.605,46.238-56.748V26.15A8.049,8.049,0,0,0,160.09,18.22Z" transform="translate(-37.443 0)" fill="#28abe2"/>
+							<path id="Path_52" data-name="Path 52" d="M36.819,18.22c14.82-2.695,27.74-8.261,38.4-16.543A7.994,7.994,0,0,1,80.13-.006V129.078a11.731,11.731,0,0,1-3.673-.588c-28.089-9.331-46.238-31.605-46.238-56.748V26.15A8.051,8.051,0,0,1,36.819,18.22Z" fill="#195da3"/>
+							<path id="Path_53" data-name="Path 53" d="M97.665,63.265c-.265-.007-.525-.04-.793-.04a30.837,30.837,0,0,0,0,61.673c.268,0,.528-.033.793-.04A232.981,232.981,0,0,0,97.665,63.265Z" transform="translate(-17.535 -30.957)" fill="#cce5ff"/>
+							<path id="Path_54" data-name="Path 54" d="M128,63.265c.267-.007.526-.04.793-.04a30.837,30.837,0,1,1,0,61.673c-.268,0-.528-.033-.793-.04Z" transform="translate(-47.874 -30.957)" fill="#d8f3ff"/>
+							<path id="Path_55" data-name="Path 55" d="M110.089,112.546l-3.475,3.475-4.691-4.691a4.274,4.274,0,0,0-6.035,0l-3.227,3.227a4.275,4.275,0,0,0,0,6.037L103.6,131.529a4.273,4.273,0,0,0,6.035,0l.456-.456C112.74,124.732,112.89,118.549,110.089,112.546Z" transform="translate(-29.959 -53.898)" fill="#5881aa"/>
+							<path id="Path_56" data-name="Path 56" d="M143.847,104.087l-3.226-3.227a4.372,4.372,0,0,0-6.037,0l-6.578,6.578v18.525l15.84-15.841A4.271,4.271,0,0,0,143.847,104.087Z" transform="translate(-47.876 -48.791)" fill="#5881aa"/>
+						</g>
+					</svg>
+					<p>Filtre af høj kvalitet i lækkert design.</p>
+				</div>
+				<div class="py-4 col-12 col-md-4">
+					<svg class="pb-4" xmlns="http://www.w3.org/2000/svg" width="116" height="116" viewBox="0 0 116 116">
+						<g id="eye-medical-svgrepo-com" transform="translate(-0.257 -0.257)">
+							<path id="Path_62" data-name="Path 62" d="M58,0A58,58,0,1,1,0,58,58,58,0,0,1,58,0Z" transform="translate(0.257 0.257)" fill="#28abe2"/>
+							<path id="Path_57" data-name="Path 57" d="M92.731,263.1,143,312.653a58.24,58.24,0,0,0,43.469-43.469L173.29,256l-28.112,16.656Z" transform="translate(-71.564 -197.743)" fill="#195da3"/>
+							<path id="Path_58" data-name="Path 58" d="M108.115,160.328a54.25,54.25,0,0,0-43.469,21.734,54.338,54.338,0,0,0,86.939,0A54.251,54.251,0,0,0,108.115,160.328Z" transform="translate(-49.859 -123.805)" fill="#fff"/>
+							<path id="Path_59" data-name="Path 59" d="M255.556,160.328l-.13,0v43.463l.13,0a54.249,54.249,0,0,0,43.469-21.734A54.251,54.251,0,0,0,255.556,160.328Z" transform="translate(-197.3 -123.805)" fill="#e2e2e2"/>
+							<circle id="Ellipse_18" data-name="Ellipse 18" cx="19.385" cy="19.385" r="19.385" transform="translate(38.872 38.872)" fill="#71e2ef"/>
+							<path id="Path_60" data-name="Path 60" d="M255.556,170.667c-.044,0-.087,0-.13,0v38.763c.044,0,.087,0,.13,0a19.385,19.385,0,1,0,0-38.769Z" transform="translate(-197.3 -131.795)" fill="#38c6d9"/>
+							<circle id="Ellipse_19" data-name="Ellipse 19" cx="9.693" cy="9.693" r="9.693" transform="translate(48.564 48.564)" fill="#273b7a"/>
+							<path id="Path_61" data-name="Path 61" d="M255.556,213.333c-.044,0-.087,0-.13,0v19.378c.043,0,.087,0,.13,0a9.692,9.692,0,1,0,0-19.385Z" transform="translate(-197.3 -164.769)" fill="#121149"/>
+						</g>
+					</svg>
+					<p>Overblik over din husholdnings vandforbrug samlet i én app.</p>
+				</div>
+				<div class="py-4 col-12 col-md-4">
+					<svg class="pb-4" xmlns="http://www.w3.org/2000/svg" width="116" height="119.65" viewBox="0 0 116 119.65">
+						<g id="money-benefit-save-profit" transform="translate(-29.55 -17.918)">
+							<path id="Path_63" data-name="Path 63" d="M84.082,72.45m-54.532,0A54.532,54.532,0,1,0,84.082,17.918,54.532,54.532,0,0,0,29.55,72.45Z" transform="translate(0 0)" fill="#28abe2"/>
+							<path id="Path_64" data-name="Path 64" d="M258.658,204.954c1.73,0,10.63-16.433,12.593-18.95.683-.865.813-1.3.3-1.929s-1.687-.588-2.967-.225c-2.4.692-5.345,0-8.649-.977-6.686-1.885-16.347-4.558-20.221,2.37-.372.675-.112.6.631,1.41,2.033,2.205,10.586,18.3,12.333,18.3Z" transform="translate(-184.859 -143.342)" fill="#a3d4ff"/>
+							<path id="Path_65" data-name="Path 65" d="M244.255,193.05h-5.976c-1.868,0-3.191-1.73-9.462-12.109-1.73-2.846-3.65-6.054-4.324-6.789a4.509,4.509,0,0,0-.355-.355,2.465,2.465,0,0,1-.536-3.313c4.653-8.32,15.776-5.189,22.409-3.278,2.915.865,5.691,1.531,7.62.977,2.318-.657,4-.372,5.008.865a3.171,3.171,0,0,1-.268,4.324c-.519.623-2.11,3.3-3.71,5.994C247.3,191.7,246.036,193.05,244.255,193.05Zm-5.406-3.9h4.9c1.418-1.514,5.388-8.139,7.577-11.806,1.5-2.508,2.595-4.324,3.33-5.449-2.889.813-6.106,0-9.7-.995-7.4-2.1-14.305-3.607-17.471.692a72.958,72.958,0,0,1,4.662,7.3C234.187,182.282,237.509,187.817,238.849,189.149Z" transform="translate(-170.456 -129.482)" fill="#2a5082"/>
+							<path id="Path_66" data-name="Path 66" d="M103.468,334.382c-6.374-3.875-12.861-2.162-16.883,0C66.537,345.15,58.45,366.037,58.45,376.295c0,20.758,16.381,26.137,36.576,26.137s36.576-5.1,36.576-25.843c0-9.972-8.614-30.332-28.135-42.207Z" transform="translate(-25.44 -276.549)" fill="#a3d4ff"/>
+							<path id="Path_67" data-name="Path 67" d="M80.717,390.06c-25.947,0-38.522-9.185-38.522-28.083,0-11.616,9.047-32.866,29.164-43.634,2.993-1.609,10.811-4.817,18.812.052,20.057,12.2,29.069,33,29.069,43.876C119.239,390.06,90.248,390.06,80.717,390.06Zm.294-70.368a16.831,16.831,0,0,0-7.784,2.084c-20.515,11-27.14,31.837-27.14,40.2,0,16.5,11,24.217,34.6,24.217,23.949,0,34.6-7.369,34.6-23.9,0-9.678-8.649-29.251-27.2-40.538a13.527,13.527,0,0,0-7.066-2.067Z" transform="translate(-11.13 -262.231)" fill="#2a5082"/>
+							<path id="Path_68" data-name="Path 68" d="M118.264,437.9h-3.892c0-8.986,6.988-25.393,22.487-33.731l1.842,3.46C123.263,415.895,118.264,431.593,118.264,437.9Zm27.936,8.519h-3.892a5.847,5.847,0,0,1-5.838-5.873h3.892a1.946,1.946,0,0,0,1.946,1.946H146.2a1.946,1.946,0,0,0,1.955-1.946v-1.055a1.946,1.946,0,0,0-1.332-1.842l-6.357-2.128a5.838,5.838,0,0,1-4-5.544v-1.081a5.847,5.847,0,0,1,5.838-5.847H146.2a5.847,5.847,0,0,1,5.838,5.847h-3.892a1.946,1.946,0,0,0-1.946-1.946h-3.892a1.946,1.946,0,0,0-1.946,1.946v1.09a1.946,1.946,0,0,0,1.332,1.851l6.357,2.119a5.829,5.829,0,0,1,4,5.544v1.047a5.847,5.847,0,0,1-5.847,5.873Z" transform="translate(-74.668 -340.011)" fill="#2a5082"/>
+							<path id="Path_69" data-name="Path 69" d="M347.74,529.376h3.892v5.838H347.74Zm0,25.315h3.892v5.847H347.74Z" transform="translate(-280.099 -450.232)" fill="#2a5082"/>
+							<path id="Path_70" data-name="Path 70" d="M624.313,187.706H628.2V199.39h-3.892Z" transform="translate(-523.564 -149.463)" fill="#d8f3ff"/>
+							<path id="Path_71" data-name="Path 71" d="M591.8,220.29h11.685v3.892H591.8Zm19.469,9.73h3.892v11.685h-3.892Z" transform="translate(-494.944 -178.147)" fill="#d8f3ff"/>
+							<path id="Path_72" data-name="Path 72" d="M721.923,334.156h11.685v3.892H721.923Z" transform="translate(-609.489 -278.382)" fill="#d8f3ff"/>
+							<path id="Path_73" data-name="Path 73" d="M540.334,496.864c-7.533,0-13.631,2.595-13.631,5.847V551.4c0,3.226,6.106,5.847,13.631,5.847s13.631-2.595,13.631-5.847V502.7C553.964,499.476,547.867,496.864,540.334,496.864Zm27.27,21.415c-7.533,0-13.631,2.595-13.631,5.847V543.6c0,3.226,6.106,5.847,13.631,5.847s13.631-2.595,13.631-5.847V524.125C581.235,520.9,575.128,518.278,567.6,518.278Z" transform="translate(-437.639 -421.612)" fill="#fff"/>
+							<path id="Path_74" data-name="Path 74" d="M526.087,563.164c-7.784,0-15.568-2.672-15.568-7.784v-9.747h3.892v9.739c0,1.375,4.437,3.892,11.685,3.892s11.685-2.526,11.685-3.892v-9.739h3.892v9.739C541.664,560.483,533.828,563.164,526.087,563.164Z" transform="translate(-423.392 -464.542)" fill="#2a5082"/>
+							<path id="Path_75" data-name="Path 75" d="M526.087,644.474c-7.784,0-15.568-2.673-15.568-7.784v-9.7h3.892v9.7c0,1.375,4.437,3.892,11.685,3.892s11.685-2.526,11.685-3.892v-9.7h3.892v9.7C541.664,641.879,533.828,644.474,526.087,644.474Z" transform="translate(-423.392 -536.157)" fill="#2a5082"/>
+							<path id="Path_76" data-name="Path 76" d="M526.087,725.608c-7.784,0-15.568-2.672-15.568-7.784v-9.773h3.892v9.773c0,1.375,4.437,3.892,11.685,3.892s11.685-2.526,11.685-3.892v-9.773h3.892v9.773C541.664,722.935,533.828,725.608,526.087,725.608Z" transform="translate(-423.392 -607.518)" fill="#2a5082"/>
+							<path id="Path_77" data-name="Path 77" d="M526.087,807.216c-7.784,0-15.568-2.672-15.568-7.784v-9.739h3.892v9.739c0,1.375,4.437,3.892,11.685,3.892s11.685-2.526,11.685-3.892v-9.739h3.892v9.739C541.664,804.543,533.828,807.216,526.087,807.216Z" transform="translate(-423.392 -679.387)" fill="#2a5082"/>
+							<path id="Path_78" data-name="Path 78" d="M526.087,544.933c-7.784,0-15.568-2.672-15.568-7.784V527.4h3.892v9.739c0,1.375,4.437,3.892,11.685,3.892s11.685-2.526,11.685-3.892V527.4h3.892v9.739C541.664,542.26,533.828,544.933,526.087,544.933Zm0-48.685c-7.784,0-15.568-2.673-15.568-7.784s7.836-7.784,15.568-7.784,15.568,2.673,15.568,7.784S533.828,496.248,526.087,496.248Zm0-11.685c-7.248,0-11.685,2.525-11.685,3.892s4.437,3.875,11.685,3.875,11.685-2.525,11.685-3.892-4.471-3.892-11.685-3.892Zm27.27,42.838c-7.784,0-15.568-2.672-15.568-7.784v-9.739h3.892v9.739c0,1.375,4.437,3.892,11.685,3.892s11.685-2.526,11.685-3.892v-9.739h3.892v9.739C568.934,524.729,561.1,527.4,553.357,527.4Z" transform="translate(-423.392 -407.365)" fill="#2a5082"/>
+							<path id="Path_79" data-name="Path 79" d="M753.891,694.68c-7.784,0-15.568-2.672-15.568-7.784v-9.739h3.892V686.9c0,1.375,4.437,3.892,11.685,3.892s11.685-2.525,11.685-3.892v-9.739h3.892V686.9c-.009,5.112-7.845,7.784-15.585,7.784Zm0-19.469c-7.784,0-15.568-2.672-15.568-7.784s7.836-7.784,15.568-7.784,15.568,2.673,15.568,7.784S761.632,675.211,753.891,675.211Zm0-11.685c-7.248,0-11.685,2.526-11.685,3.892s4.437,3.892,11.685,3.892,11.685-2.526,11.685-3.892S761.139,663.526,753.891,663.526Z" transform="translate(-623.927 -564.905)" fill="#2a5082"/>
+						</g>
+					</svg>
+					<p>Du sparer penge på din vandregning.</p>
+				</div>
+			</div>
+		</div>
+</section>
+
 
 
 
